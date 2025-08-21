7b6ccc1427781411a3b403987d429d6a876c540f
devopsbyrohit09 <devopsbyrohit09@gmail.com>
2025-08-21 13:06:27 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,26 +1,26 @@
-resource "aws_iam_user" "iam-users" {
+resource "aws_iam_user" "users" {
   for_each = toset(local.users)
 
   name = each.value
 }
 
 locals {
-  users = ["User-1", "User-2", "User-3"]
+  users = ["fresher-1", "fresher-2", "fresher-3"]
 }
 
 # Attach S3 ReadOnly Policy
 resource "aws_iam_user_policy_attachment" "s3_readonly" {
   for_each = toset(local.users)
 
-  user       = aws_iam_user.iam-users[each.value].name
+  user       = aws_iam_user.users[each.value].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
 # Attach EC2 ReadOnly Policy
 resource "aws_iam_user_policy_attachment" "ec2_readonly" {
-  for_each = toset(local.iam-users)
+  for_each = toset(local.users)
 
-  user       = aws_iam_user.iam-users[each.value].name
+  user       = aws_iam_user.users[each.value].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
